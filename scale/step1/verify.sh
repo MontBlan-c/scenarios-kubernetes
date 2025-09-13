67a712f37d57f23e7c79fdef32b9f419358fb3ae
MontBlan-c <zoe@siegel.io>
2025-09-13 20:50:40 +0900

    Refactor verify.sh to improve deployment checks

--- scale/step1/verify.sh
+++ scale/step1/verify.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-kubectl get deploy my-deploy -o jsonpath='{.spec.replicas}' 2>/dev/null | grep -qx 3 \
-  && kubectl get deploy my-deploy -o jsonpath='{.spec.template.spec.containers[0].image}' \
-     | grep -qx nginx \
-  && exit 1
+set -e
+
+# Fail if the deployment is missing, wrong replica count, or wrong image
+kubectl get deployment my-deploy >/dev/null 2>&1 &&
+[ "$(kubectl get deployment my-deploy -o jsonpath='{.spec.replicas}')" = "3" ] &&
+[ "$(kubectl get deployment my-deploy -o jsonpath='{.spec.template.spec.containers[0].image}')" = "nginx" ]
 
